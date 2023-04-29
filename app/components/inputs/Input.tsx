59f635cfb1a6859dce3df7180d70986303df7022
Lukyord <ttansiri524@gmail.com>
2023-04-29 21:26:35 +0200

    implementing credentials login/logout

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modal/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -12,18 +14,21 @@ export const metadata = {
   description: "Airbnb Clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
-          <Navbar />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
