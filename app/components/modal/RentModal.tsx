a790fc5eb86b3e9a6c292fcfca512870612d7a7e
Lukyord <ttansiri524@gmail.com>
2023-04-30 15:03:18 +0200

    add map ui and location select

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -47,6 +48,14 @@ export default function RentModal() {
 
   const category = watch("category");
   const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -121,6 +130,7 @@ export default function RentModal() {
           value={location}
           onChange={(value) => setCustomValue("location", value)}
         />
+        <Map center={location?.latlng} />
       </div>
     );
   }
